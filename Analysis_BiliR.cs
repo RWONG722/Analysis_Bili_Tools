3c1f9df0bddd4ae894d7606b74628a4d45b6b7e5
Raymond WONG <54637627+RWONG722@users.noreply.github.com>
2025-01-01 09:20:44 +0000

    脚本更新

--- Analysis_BiliR.cs
+++ Analysis_BiliR.cs
@@ -1,6 +1,6 @@
 ﻿/**
 Script Name: Analysis_BiliR
-Version: 2.0.2
+Version: 2.0.3 Update on 2025/01/01
 Description: B站视频解析工具
 Author: Raymond_OuO(github.com/RWONG722)
 https://github.com/RWONG722/Analysis_Bili_Tools
@@ -14,120 +14,103 @@ using VRC.Udon;
 
 public class Analysis_BiliR : UdonSharpBehaviour
 {
-
     [Header("解析链接组成：解析地址 + B站URL + BV号 + 后缀")]
-
     [Header("解析地址")]
     [SerializeField] private string server1;
     [SerializeField] private string server2;
 
     [Header("B站URL前缀")]
-    [SerializeField] private string prefix;
+    [SerializeField] private string prefix1 = "bilibili.com/";
+    [SerializeField] private string prefix2 = "bilibili.com/";
 
     [Header("URL后缀")]
-    [SerializeField] private string suffix;
+    [SerializeField] private string suffix1;
+    [SerializeField] private string suffix2;
 
     [Header("输入框")]
-    public GameObject ui;
-    public InputField SJX_Url;
-    public InputField Sprefix;
-    public InputField Ssuffix;
-    public InputField Input;
-    public InputField part;
+    public GameObject SettingUi;
+    public InputField APIUrlInput;
+    public InputField PrefixInput;
+    public InputField SuffixInput;
+    public InputField VideoUrlInput;
+    public InputField VideoPartInput;
+
+    private const string defaultAPI = "https://api.rwit.net/?url=";
 
     void Start()
-    {   
-	//default Api Server
-		string defaultAPI = "https://api.rwit.net/?url=";
-        part.text = "";
-		
-        if (server1.Length > 0)
-        {	
-            SJX_Url.text = server1;
-        }
-        else if (server2.Length > 0)
-        {   
-            SJX_Url.text = server2;
-        }
-        else
-        {
-            SJX_Url.text = defaultAPI;
-        }
-
-        if (prefix.Length > 0)
-        {
-            Sprefix.text = prefix;
-        }
-        else
-        {
-            Sprefix.text = "bilibili.com/";
-        }
-
-        if (server1.Length == 0)
-        {
-            server1 = defaultAPI;
-        }
-        if (server2.Length == 0)
-        {
-            server2 = defaultAPI;
-        }
-        
-
-    }
-    public void OnInput()//输入框内容变化时
     {
-		if (SJX_Url.text.Length > 0)
-		{
-			Input.text = "没有填入解析服务器！";
-            return;
-		}
-		
-        if (Input.text.Contains("BV") || Input.text.Length > 11)
+        InitializeFields();
+    }
+
+    private void InitializeFields()
+    {
+        APIUrlInput.text = !string.IsNullOrEmpty(server1) ? server1 : !string.IsNullOrEmpty(server2) ? server2 : defaultAPI;
+    }
+
+    public void OnInput() // 输入框内容变化时
+    {
+        if (string.IsNullOrEmpty(APIUrlInput.text))
         {
-            string text = Input.text;
-            Input.text = Input.text.Substring(Input.text.IndexOf("BV"), 12);
-            string jxurl = SJX_Url.text + Sprefix.text + Input.text + Ssuffix.text;
-            Input.text = jxurl;
-            
+            VideoUrlInput.text = "没有填入解析服务器！";
+            return;
+        }
 
-            //part is number and need > 1
-            if (part.text.Length > 0 && int.Parse(part.text) > 1)
+        if (VideoUrlInput.text.Contains("BV") || VideoUrlInput.text.Length > 11)
+        {
+            string bvNumber = ExtractBVNumber(VideoUrlInput.text);
+            string jxurl = $"{APIUrlInput.text}{PrefixInput.text}{bvNumber}{SuffixInput.text}";
+            VideoUrlInput.text = jxurl;
+
+            // part is number and need > 1
+            if (int.TryParse(VideoPartInput.text, out int partNumber) && partNumber > 1)
             {
-                Input.text += "&part=" + part.text;
+                VideoUrlInput.text += $"&part={partNumber}";
             }
-
         }
         else
         {
             OnClear();
         }
-
     }
 
-    public void OnClear()//清空输入框
+    private string ExtractBVNumber(string input)
     {
-        Input.text = "";
-        Input.placeholder.GetComponent<Text>().text = "请输入B站链接或BV号!";
+        int startIndex = input.IndexOf("BV");
+        return startIndex >= 0 ? input.Substring(startIndex, 12) : string.Empty;
+    }
 
+    public void OnClear() // 清空输入框
+    {
+        VideoUrlInput.text = "";
+        VideoUrlInput.placeholder.GetComponent<Text>().text = "请输入B站链接或BV号!";
     }
 
     public void Click_Setting()
     {
-        ui.SetActive(!ui.activeSelf);
+        SettingUi.SetActive(!SettingUi.activeSelf);
     }
 
     public void OnSelect_Server1()
     {
-        SJX_Url.text = server1;
+        UpdateServerSettings(suffix1, prefix1, server1);
     }
 
     public void OnSelect_Server2()
     {
-        SJX_Url.text = server2;
+        UpdateServerSettings(suffix2, prefix2, server2);
+    }
+
+    private void UpdateServerSettings(string suffix, string prefix, string server)
+    {
+        SuffixInput.text = suffix;
+        PrefixInput.text = prefix;
+        APIUrlInput.text = server;
     }
 
     public void OnSelect_Clear()
     {
-        SJX_Url.text = "";
+        SuffixInput.text = "";
+        PrefixInput.text = "";
+        APIUrlInput.text = "";
     }
 }
